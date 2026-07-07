772c14e2fb705aea052f8ff71f90ce3422ab7229
AniP-gt <neko.basu775@gmail.com>
2026-07-07 15:54:56 +0900

    feat: use complete instrument filter options
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- ui/src/components/AppMainPane/AppMainPane.tsx
+++ ui/src/components/AppMainPane/AppMainPane.tsx
@@ -45,6 +45,7 @@ export function AppMainPane({
 }: AppMainPaneProps) {
   const { favorites: sampleFavorites } = useFavoritesStore();
   const { favorites: midiFavorites } = useMidiFavoritesStore();
+  const instrumentTypeOptions = sampleState.instrumentTypes.map((type) => type.name) as Sample["instrument_type"][];
   
   return (
     <div
@@ -173,6 +174,7 @@ export function AppMainPane({
         <SampleList
           ref={sampleListRef}
           samples={displayedSamples}
+          instrumentTypeOptions={instrumentTypeOptions}
           samplePaths={sampleState.samplePaths}
           filters={sampleState.filters}
           sort={sampleState.sort}
@@ -284,7 +286,7 @@ export function AppMainPane({
           filters={sampleState.filters}
           onFilterChange={sampleState.handleFilterChange}
           allInstrumentTypeNames={
-            sampleState.instrumentTypes.map((t) => t.name) as import("../../types/sample").InstrumentType[]
+            instrumentTypeOptions
           }
           onError={(message) => {
             scanState.setError(message);
